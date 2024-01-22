4d5fba7c784c43b055ed9eb91c5755151b53d9bc
Holysqualor <149847559+Holysqualor@users.noreply.github.com>
2024-01-22 19:49:08 +0100

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -1,8 +1,18 @@
 public enum Direction {
-    FORWARD,
-    BACK,
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
+    FORWARD(1, 0, 0),
+    BACK(-1, 0, 0),
+    LEFT(0, 1, 0),
+    RIGHT(0, -1, 0),
+    UP(0, 0, 1),
+    DOWN(0, 0, -1);
+
+    private final Vector vector;
+
+    Direction(double x, double y, double z) {
+        vector = new Vector(x, y, z);
+    }
+
+    public Vector getVector() {
+        return vector;
+    }
 }
