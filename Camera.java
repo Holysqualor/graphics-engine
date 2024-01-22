ba0c5fa41e72f0c25074c126628ffcd5a6fcea9d
Holysqualor <149847559+Holysqualor@users.noreply.github.com>
2024-01-22 19:48:53 +0100

    Update Camera.java

--- Camera.java
+++ Camera.java
@@ -1,9 +1,11 @@
 public class Camera extends DynamicObject {
+    private static final double START_SPEED = 0.1;
+
     public Camera(double x, double y, double z) {
-        super(new Vector(x,y,z), 0.1);
+        super(new Vector(x,y,z), START_SPEED);
     }
 
     public Camera() {
-        super(new Vector(0,0,0), 0.1);
+        super(new Vector(0,0,0), START_SPEED);
     }
 }
