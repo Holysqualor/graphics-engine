9ab615f197e5afb524eea7b29613daa7b0e5380f
Holysqualor <149847559+Holysqualor@users.noreply.github.com>
2024-01-30 22:05:31 +0100

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -1,55 +1,73 @@
-
 public class Vector {
-    private double x;
-    private double y;
-    private double z;
+    private double x, y, z;
 
     public Vector(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
+
     }
 
-    public Vector(Vector vector) {
-        this.x = vector.getX();
-        this.y = vector.getY();
-        this.z = vector.getZ();
+    public Vector negate() {
+        return new Vector(-x, -y, -z);
     }
 
-    public Vector(double h, double v) {
-        x = Math.cos(h) * Math.cos(v);
-        y = Math.sin(h) * Math.cos(v);
-        z = Math.sin(v);
+    public Vector copy() {
+        return new Vector(x, y, z);
     }
 
-    public Vector(double h) {
-        x = Math.cos(h);
-        y = Math.sin(h);
+    public void add(Vector other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+    }
+
+    public void mul(double k) {
+        x *= k;
+        y *= k;
+        z *= k;
+    }
+
+    public void normalize() {
+        double len = length();
+        if(len != 0) {
+            x /= len;
+            y /= len;
+            z /= len;
+        }
+    }
+
+    public Vector cross(Vector other) {
+        return new Vector(
+                y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x
+        );
+    }
+
+    public void rotate(Vector other, double theta) {
+        double cosTheta = Math.cos(theta);
+        double sinTheta = Math.sin(theta);
+
+        double dotProduct = x * other.x + y * other.y + z * other.z;
+        Vector crossProduct = cross(other);
+
+        x = cosTheta * x + (1 - cosTheta) * dotProduct * other.x + sinTheta * crossProduct.x;
+        y = cosTheta * y + (1 - cosTheta) * dotProduct * other.y + sinTheta * crossProduct.y;
+        z = cosTheta * z + (1 - cosTheta) * dotProduct * other.z + sinTheta * crossProduct.z;
+        normalize();
+    }
+
+    public double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public boolean isOrthogonal(Vector other) {
+        return Math.round(x * other.x + y * other.y + z * other.z) == 0;
+    }
+
+    void ignoreHeight() {
         z = 0;
-    }
-
-    public void add(Vector vector) {
-        x += vector.getX();
-        y += vector.getY();
-        z += vector.getZ();
-    }
-
-    public void setSpeed(double speed) {
-        x *= speed;
-        y *= speed;
-        z *= speed;
-    }
-
-    public void setPosition(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public void setPosition(Vector other) {
-        this.x = other.getX();
-        this.y = other.getY();
-        this.z = other.getZ();
     }
 
     public double getX() {
