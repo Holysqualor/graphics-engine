d353756d96b6a147ac8167b77cacedfc8209eade
Holysqualor <149847559+Holysqualor@users.noreply.github.com>
2024-02-11 12:59:58 +0100

    Create Settings.java

---
+++ Settings.java
@@ -0,0 +1,7 @@
+public class Settings {
+    public static int SCREEN_WIDTH = 1280;
+    public static int SCREEN_HEIGHT = 720;
+    public static final float SENSITIVITY = 30f;
+    public static final float DRAW_DISTANCE = 10f;
+    public static final float FOV = 0.8f;
+}
